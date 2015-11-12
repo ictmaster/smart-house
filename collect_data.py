245e6c20eb3a99725fae4819b23a163cd30c8c65
Jonas Natten <jonasn12@student.uia.no>
2015-11-12 15:10:01 +0100

    Updates to python 3

--- collect_data.py
+++ collect_data.py
@@ -1,26 +1,24 @@
 #!/usr/bin/env python
 import os
-import gs2
 import time
-import json
+
+import gs2
 
 startTime = time.time()
 
 FILE_LOCATION = "./GS2-filer/"
 
-all_files = [f for f in os.listdir(FILE_LOCATION) if os.path.isfile(os.path.join(FILE_LOCATION, f))] #gets all files in folder
-files = [f for f in all_files if f.startswith("uke")] #gets files that starts with uke
+all_files = [f for f in os.listdir(FILE_LOCATION) if
+             os.path.isfile(os.path.join(FILE_LOCATION, f))]  # gets all files in folder
+files = [f for f in all_files if f.startswith("uke")]  # gets files that starts with uke
 
 gs2s = []
 for f in files:
-	data_object = gs2.GS2File()
-	data_object.ProcessFile(FILE_LOCATION+f)
-	gs2s.append(data_object)
-	
-#gs2.loadJson(gs2s, "uke_data.json")
-gs2.saveJson(gs2s, "uke_data.json")
+    data_object = gs2.GS2File()
+    data_object.process_file(FILE_LOCATION + f)
+    gs2s.append(data_object)
 
+gs2.load_json(gs2s, "uke_data.json")
+# gs2.save_json(gs2s, "uke_data.json")
 
-
-
-print "Script took",time.time()-startTime,"seconds..."
+print("Script took", time.time() - startTime, "seconds...")
--- gs2.py
+++ gs2.py
@@ -2,74 +2,79 @@
 import os
 import json
 
-def loadJson(gs2_list, filename):
-	del gs2_list[:]
-	with open(filename, 'r') as json_file:
-		data = json.load(json_file)
-		for f in data:
-			tmpFile = GS2File()
-			tmpFile.name = f["name"]
-			
-			for sec in f["sections"]:
-				tmpSection = GS2Section()
-				tmpSection.name = sec["name"]
-				for propName, propValue in sec["properties"].iteritems():
-					tmpSection.properties[propName] = propValue
-				tmpFile.sections.append(tmpSection)
-			gs2_list.append(tmpFile)
 
-def saveJson(gs2_list, filename):
-	with open(filename, "w") as json_file:
-		json_file.write(str(gs2_list))
+def load_json(gs2_list, filename):
+    del gs2_list[:]
+    with open(filename, 'r') as json_file:
+        data = json.load(json_file)
+        for f in data:
+            tmp_file = GS2File()
+            tmp_file.name = f["name"]
+
+            for sec in f["sections"]:
+                tmp_section = GS2Section()
+                tmp_section.name = sec["name"]
+                for propName, propValue in sec["properties"].items():
+                    tmp_section.properties[propName] = propValue
+                tmp_file.sections.append(tmp_section)
+            gs2_list.append(tmp_file)
+
+
+def save_json(gs2_list, filename):
+    with open(filename, "w") as json_file:
+        json_file.write(str(gs2_list))
+
 
 class GS2File(object):
-	def __init__(self):
-		self.name = None
-		self.sections = []
+    def __init__(self):
+        self.name = None
+        self.sections = []
 
-	def __repr__(self):
-		retStr = "{\"name\":\""+str(self.name)+"\", \"sections\":["
-		for sect in self.sections[:-1]:
-			retStr += str(sect) + ","
-		retStr += str(self.sections[-1])
-		retStr += "]}"
+    def __repr__(self):
+        ret_str = "{\"name\":\"" + str(self.name) + "\", \"sections\":["
+        for sect in self.sections[:-1]:
+            ret_str += str(sect) + ","
+        ret_str += str(self.sections[-1])
+        ret_str += "]}"
 
-		return retStr
+        return ret_str
 
-	def ProcessFile(self, filename):
-		f = open(filename, 'r') # Open the file so we can read it
-		self.name = unicode(os.path.split(filename)[1].strip(),'utf-8') # Set name of object
-		currentSection = None
-		curLine = 1
-		maxLine = 100
-		for line in f:
+    def process_file(self, filename):
+        f = open(filename, 'r', encoding="latin-1")  # Open the file so we can read it
+        self.name = str(os.path.split(filename)[1].strip())  # Set name of object
+        current_section = None
+        current_line = 1
+        max_line = 100
+        for line in f:
 
-			if curLine > maxLine:
-				pass
-				#break
+            if current_line > max_line:
+                pass
+                # break
 
-			# Skip blank lines
-			if line.strip() == "":
-				continue
+            # Skip blank lines
+            if line.strip() == "":
+                continue
 
-			# Add section to file object
-			if line.startswith("##"):
-				if currentSection != None:
-					self.sections.append(currentSection)
-				
-				currentSection = GS2Section()
-				currentSection.name = unicode(line[2:].strip(),'utf-8') # Get Sectionname
-			else:
-				#Add property to section
-				# print "name: '" + line[1:].split('=')[0].strip()+"', value: '"+ line[1:].split('=')[1].strip()+"'"
-				currentSection.properties[unicode(line[1:].split('=')[0].strip(), 'utf-8', 'replace')] = unicode(line[1:].split('=')[1].strip(),'utf-8', 'replace')
-			curLine+=1
-		f.close()
+            # Add section to file object
+            if line.startswith("##"):
+                if current_section is not None:
+                    self.sections.append(current_section)
+
+                current_section = GS2Section()
+                current_section.name = str(line[2:].strip())  # Get Sectionname
+            else:
+                # Add property to section
+                # print "name: '" + line[1:].split('=')[0].strip()+"', value: '"+ line[1:].split('=')[1].strip()+"'"
+                current_section.properties[str(line[1:].split('=')[0].strip())] = str(
+                    line[1:].split('=')[1].strip())
+            current_line += 1
+        f.close()
+
 
 class GS2Section(object):
-		def __init__(self):
-			self.name = None
-			self.properties = {}
+    def __init__(self):
+        self.name = None
+        self.properties = {}
 
-		def __repr__(self):
-			return json.dumps(self.__dict__)
+    def __repr__(self):
+        return json.dumps(self.__dict__)
